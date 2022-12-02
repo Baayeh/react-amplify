16b42cafbd51adf4a21e1c3527a7559b5ff59660
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-02 04:33:11 +0000

    Update

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,6 +3,8 @@ export type AmplifyDependentResourcesAttributes = {
         "reactamplifyd79ebf97": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
